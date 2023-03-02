f694effa4d43e817a6803c02b62ff3f55feeaebc
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2023-03-02 12:29:16 +0100

    Configure iam file with policies arns

--- iam.tf
+++ iam.tf
@@ -18,12 +18,12 @@ POLICY
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  policy_arn = var.AMAZON_EKS_CLUSTER_POLICY_ARN
   role       = aws_iam_role.eks_cluster.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  policy_arn = var.AMAZONEKS_SERVICE_POLICY_ARN
   role       = aws_iam_role.eks_cluster.name
 }
 
@@ -47,16 +47,16 @@ POLICY
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  policy_arn = var.AMAZONEKS_WORKERNODE_POLICY_ARN
   role       = aws_iam_role.eks_nodes.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  policy_arn = var.AMAZONEKS_CNI_POLICY_ARN
   role       = aws_iam_role.eks_nodes.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  policy_arn = var.AMAZONEC2_CONTAINER_REGISTRY_READONLY_ARN
   role       = aws_iam_role.eks_nodes.name
 }
