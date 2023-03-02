1343f21592f0452afc8b041268245571f1bf9bca
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2023-03-02 11:32:23 +0100

    Configure VPC module

--- main.tf
+++ main.tf
@@ -14,17 +14,13 @@ provider "http" {
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.64.0"
-
-  name = "vpc-module-demo"
-  cidr = "10.0.0.0/16"
-
+  name = var.VPC_NAME
+  cidr = var.VPC_CIDR_BLOCK
   azs             = slice(data.aws_availability_zones.available.names, 0, 2)
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
+  private_subnets = var.VPC_PRIVATE_SUBNETS
+  public_subnets  = var.VPC_PUBLIC_SUBNETS
   enable_nat_gateway = false
   enable_vpn_gateway = false
-
   tags = {
     Name = "${var.CLUSTER-NAME}-vpc"
   }
