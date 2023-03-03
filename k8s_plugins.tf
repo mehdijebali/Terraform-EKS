3c015047a4da817e75c64cb19c1b079063095e44
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2023-03-03 12:21:15 +0100

    Apply external-dns k8s manifest

--- k8s_plugins.tf
+++ k8s_plugins.tf
@@ -5,3 +5,11 @@ resource "kubectl_manifest" "certmanager" {
     aws_eks_node_group.node
   ]
 }
+
+resource "kubectl_manifest" "externaldns" {
+  yaml_body = file("./k8s_manifests/external-dns.yaml")
+  depends_on = [
+    aws_eks_cluster.aws_eks,
+    aws_eks_node_group.node
+  ]
+}
