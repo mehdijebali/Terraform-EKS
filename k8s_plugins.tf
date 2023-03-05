e5262388aff1af39f17df555dbca22d2aa7848d4
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2023-03-05 15:58:59 +0100

    Create K8s serviceaccount: lbc-serviceaccount

--- k8s_plugins.tf
+++ k8s_plugins.tf
@@ -13,3 +13,22 @@ resource "kubectl_manifest" "externaldns" {
     aws_eks_node_group.node
   ]
 }
+
+resource "kubectl_manifest" "lbc-serviceaccount" {
+  yaml_body = <<YAML
+  apiVersion: v1
+  kind: ServiceAccount
+  metadata:
+    labels:
+      app.kubernetes.io/component: controller
+      app.kubernetes.io/name: aws-load-balancer-controller
+    name: aws-load-balancer-controller
+    namespace: kube-system
+    annotations:
+      eks.amazonaws.com/role-arn: arn:aws:iam::${var.AWS_ACCOUNT_ID}:role/AmazonEKSLoadBalancerControllerRole
+  YAML
+  depends_on = [
+    aws_eks_cluster.aws_eks,
+    aws_eks_node_group.node
+  ]
+}
