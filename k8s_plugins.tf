b32cbe73e2349907ee8a2b7d64507b4a1b3582f3
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2023-03-10 10:31:27 +0100

    create service account: efs-service-account

--- k8s_plugins.tf
+++ k8s_plugins.tf
@@ -33,6 +33,24 @@ resource "kubectl_manifest" "lbc-serviceaccount" {
   ]
 }
 
+resource "kubectl_manifest" "efs-service-account" {
+  yaml_body = <<YAML
+  apiVersion: v1
+  kind: ServiceAccount
+  metadata:
+    labels:
+      app.kubernetes.io/name: aws-efs-csi-driver
+    name: efs-csi-controller-sa
+    namespace: kube-system
+    annotations:
+      eks.amazonaws.com/role-arn: ${aws_iam_role.AmazonEKS_EFS_CSI_DriverRole.arn}
+  YAML
+  depends_on = [
+    aws_eks_cluster.aws_eks,
+    aws_eks_node_group.node
+  ]
+}
+
 resource "kubectl_manifest" "lbc" {
   yaml_body = file("./k8s_manifests/lb-controller.yaml")
   depends_on = [
